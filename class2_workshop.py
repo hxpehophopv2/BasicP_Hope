1cbc0650136fd9f62012c1a350de5289ba993a32
yh <teeruch.song@kmutt.ac.th>
2025-07-22 16:05:53 +0700

    Update class_workshop.py

--- class2_workshop.py
+++ class2_workshop.py
@@ -1,14 +1,14 @@
 d = int(input("Distance (km): "))
 
-if d >= 500:
+if d >= 500 :
     print("Your shipment cost is ฿45.")
 elif d >= 301:
     print("Your shipment cost is ฿35.")
-elif d >= 100:
+elif d >= 101:
     print("Your shipment cost is ฿25.")
 elif d >= 51:
     print("Your shipment cost is ฿15.")
 elif d >= 5:
     print("Your shipment cost is ฿10.")
 else:
-    print("We do not accept shipment at this distance.")
+    print("We do not offer shipment at this distance.")
