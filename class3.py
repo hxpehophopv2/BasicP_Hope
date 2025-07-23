9a81fe145c0e8c7427a5c50267f1405c9f7b54cb
hxpehophopv2 <teeruch.song@kmutt.ac.th>
2025-07-23 11:17:35 +0700

    Added class3.py

---
+++ class3.py
@@ -0,0 +1,65 @@
+# hasRice = True
+# hasSpoon = False
+# hasHand = True
+
+# if hasRice:
+#     if hasSpoon:
+#         print("Eat rice")
+#     elif hasHand:
+#         print("Eat sticky rice")
+
+# score = float(input("Input your score: "))
+# if score >= 0:
+#     if score <= 100:
+#         if score >= 0:
+#             if score >= 50:
+#                 if score >= 60:
+#                     if score >= 70:
+#                         if score >= 80:
+#                             print("You've got an A grade.")
+#         if score >= 0:
+#             if score >= 50:
+#                 if score >= 60:
+#                     if score >= 70:
+#                         if score < 80:
+#                             print("You've got a B grade.")
+#         if score >= 0:
+#             if score >= 50:
+#                 if score >= 60:
+#                     if score < 70:
+#                         print("You've got a C grade.")
+#         if score >= 0:
+#             if score >= 50:
+#                 if score < 60:
+#                     print("You've got a D grade.")
+#         if score >= 0:
+#             if score < 50:
+#                 print("You've failed (F Grade).")
+# if score < 0:
+#     print("Error: False score.")
+# if score > 100:
+#     print("Error: False score.")
+
+# for i in range(7):
+#     print(i)
+
+# i = 0
+# while i < 5:
+#     print("Hi")
+#     i += 1
+
+# while True:
+#     choice = int(input("Input 1 to add numbers, 2 to quit: "))
+
+#     if choice == 1:
+#         num = int(input("The amount of numbers you wish to add: "))
+#         sum = 0
+
+#         for i in range(num):
+#             num1 = int(input("Input a number: "))
+#             sum = sum + num1
+#         print("Addition results", sum)
+    
+#     if choice == 2:
+#         print("Quitting...")
+#         break
